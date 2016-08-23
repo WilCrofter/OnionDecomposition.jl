276f0de8d007b76ac51b3144363cd6d40f350eae
wilcrofter <bill@crofter.net>
2016-08-23 18:45:11 -0400

    untested OD implementation

--- src/algo.jl
+++ src/algo.jl
@@ -9,5 +9,26 @@ typealias AdjMatrix{T<:Real} Union{Matrix{T},SparseMatrixCSC{T,Int}}
 Performs an onion decomposition of the given adjacency matrix.
 """
 function OD{T}(A::AdjMatrix{T})
-    
+    m, n = size(A)
+    m == n || error("Adjacency matrix must be square")
+    A == A' || error("Adjaceny matrix must be symmetric")
+    V = collect(1:m)
+    Vidx = trues(m)
+    Lidx = trues(m)
+    corelist = zeros(Int,m)
+    layerlist = zeros(Int,m)
+    core = layer = 1
+    while any(Vidx)
+        corelist[Vidx] = core
+        # Recursively remove nodes of degree <= core, recalculating degrees,
+        # until there are none left, then increment core and repeat.
+        while any(Lidx)
+            Lidx = deepcopy(Vidx)
+            Lidx &= sum(A[Lidx,Lidx],2) .<= core
+            layerlist[Lidx] = layer
+            Vidx &= Lidx
+            layer += 1
+        end
+        core += 1
+    end
 end
