ae383b93c9fdea9f80c20a214b7630357a753cfd
wilcrofter <bill@crofter.net>
2016-08-24 17:34:56 -0400

    OD algorithm apparently working

--- src/algo.jl
+++ src/algo.jl
@@ -13,8 +13,8 @@ function OD{T}(A::AdjMatrix{T})
     m == n || error("Adjacency matrix must be square")
     A == A' || error("Adjaceny matrix must be symmetric")
     V = collect(1:m)
-    Vidx = trues(m)
-    Lidx = trues(m)
+    Vidx = fill(true,m)
+    Lidx = fill(true,m)
     corelist = zeros(Int,m)
     layerlist = zeros(Int,m)
     core = layer = 1
@@ -22,13 +22,26 @@ function OD{T}(A::AdjMatrix{T})
         corelist[Vidx] = core
         # Recursively remove nodes of degree <= core, recalculating degrees,
         # until there are none left, then increment core and repeat.
-        while any(Lidx)
-            Lidx = deepcopy(Vidx)
-            Lidx &= sum(A[Lidx,Lidx],2) .<= core
-            layerlist[Lidx] = layer
-            Vidx &= Lidx
-            layer += 1
+        #
+        while true
+            # Initialize layer indices to those of all remaining vertices
+            Lidx[1:m] = Vidx[1:m] # modifies Lidx in place
+            println("\n", core, ",", layer)
+            # Reduce layer indices to those of all vertices with
+            # degree <= core in the current subgraph
+            Lidx[Vidx] &= (sum(A[Vidx,Vidx],2) .<= core)
+            # If such vertices exist
+            if any(Lidx)
+                # Assign the current layer to them.
+                layerlist[Lidx] = layer
+                # Remove them from the graph
+                Vidx[Lidx] = false
+                layer += 1
+            else
+                break;
+            end
         end
         core += 1
     end
+    return corelist, layerlist
 end
--- src/util.jl
+++ src/util.jl
@@ -13,8 +13,15 @@
 """
 function fig1()
     # v0[i] and v1[i] define an adjacency (edge)
-    v0 = [1, 2, 3, 4, 9, 9, 5, 6, 7, 7, 8, 8, 8, 8,15,15]
-    v1 = [2, 3, 4, 9,10,11, 6, 7,12,13,12,13,14,15,16,17]
+    v0 = [1, 2, 3, 4, 9, 9, 4, 5, 6, 7, 7,13, 7, 8, 8, 8, 8,15,15]
+    v1 = [2, 3, 4, 9,10,11, 5, 6, 7,12,13,14, 8,12,13,14,15,16,17]
     # Since the graph is undirected, v1[i] and v0[i] are also adjacent
     return sparse(vcat(v0,v1), vcat(v1,v0), ones(Int,length(v0)+length(v1)))
+end
+
+function fig1_OD()
+    tmp  =   [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17]
+    cores =  [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1]
+    layers = [1, 2, 3, 4, 5, 6, 8, 8, 2, 1, 1, 7, 8, 7, 2, 1, 1]
+    return cores, layers
 end
